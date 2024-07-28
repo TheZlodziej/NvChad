893f8f324264c11b56ac40ee54c12c357c2e7437
minekuba <galaxykuby@gmail.com>
2024-07-28 12:24:09 +0200

    replace all mappings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -11,6 +11,28 @@ M.dap = {
       "<cmd> DapContinue <CR>",
       "Start or continue the debugger",
     }
+  },
+}
+
+M.general = {
+  n = {
+    ["<C-r>r"] = {
+      function()
+        local current_word = vim.fn.expand("<cword>")
+        vim.ui.input(
+          { prompt="Replace \"" .. current_word .. "\" with (esc to cancel): "},
+          function(replacement_word)
+            if replacement_word == nil then
+              return
+            end
+            current_word = vim.fn.escape(current_word, "\\")
+            replacement_word = vim.fn.escape(replacement_word, "\\")
+            vim.cmd("%s/\\<" .. current_word .. "\\>/" .. replacement_word .. "/g")
+          end
+        ) 
+      end,
+      { noremap = false, silent = true }
+    }
   }
 }
 
